9f9068c88d3e3a30285ea6af24e397338790897b
Sacha Weatherstone <sachaw100@hotmail.com>
2024-05-20 12:56:50 +1000

    format and cleanup

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    extensions: ['ts', 'js'],
-    ignore: ['node_modules'],
-    recursive: true,
-    require: ['ts-node/register'],
-    spec: ["test/**/*.spec.*", "test/**/**/*.spec.*"]
-};
---
+++ lib/index.ts
@@ -0,0 +1,31 @@
+/**
+ * This allows for imports from @ngageoint/mgrs-js
+ * @since 1.0.0
+ */
+export * from "./MGRS.js";
+export * from "./MGRSConstants.js";
+export * from "./MGRSUtils.js";
+
+export * from "./features/GridLine.js";
+
+export * from "./grid/Grid.js";
+export * from "./grid/GridLabel.js";
+export * from "./grid/GridLabeler.js";
+export * from "./grid/GridType.js";
+export * from "./grid/GridTypeUtils.js";
+export * from "./grid/Grids.js";
+export * from "./grid/MGRSLabeler.js";
+export * from "./grid/ZoomGrids.js";
+
+export * from "./gzd/BandLetterRange.js";
+export * from "./gzd/GZDLabeler.js";
+export * from "./gzd/GridRange.js";
+export * from "./gzd/GridZone.js";
+export * from "./gzd/GridZones.js";
+export * from "./gzd/LatitudeBand.js";
+export * from "./gzd/LongitudinalStrip.js";
+export * from "./gzd/ZoneNumberRange.js";
+
+export * from "./property/MGRSProperties.js";
+
+export * from "./utm/UTM.js";
